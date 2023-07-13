d9a1dec42ffa4cbc64d34e94dd27c8eba0f15586
Toyehamzat <anibiseun@gmail.com>
2023-07-13 16:33:10 +0100

    updated repo

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -24,100 +24,190 @@ export function Footer() {
 
         <div id="footer-contact">
           <h4>SOCIAL</h4>
-          <h4>INITIATIVES</h4>
+          <h4>PRODUCTS</h4>
           <h4>OFFICES</h4>
           <ul>
             <li>
-              <a id="link" href="#">
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
                 Instagram
               </a>
             </li>
             <li>
-              <a id="link" href="#">
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
                 Twitter
               </a>
             </li>
             <li>
-              <a id="link" href="#">
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
                 LinkedIn
               </a>
             </li>
             <li>
-              <a id="link" href="#">
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
                 Facebook
               </a>
             </li>
             <li>
-              <a id="link" href="#">
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
                 Github
               </a>
             </li>
           </ul>
           <ul>
             <li>
-              <a id="link" href="#">
-                Crafted
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
+                Airpods
               </a>
             </li>
             <li>
-              <a id="link" href="#">
-                Applied
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
+                macbook Air
               </a>
             </li>
             <li>
-              <a id="link" href="#">
-                Bandbeats
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
+                Macbook pro
               </a>
             </li>
             <li>
-              <a id="link" href="#">
-                Moves
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
+                iphone
               </a>
             </li>
             <li>
-              <a id="link" href="#">
-                B®/Good
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
+                ipad
               </a>
             </li>
           </ul>
           <ul>
             <li>
-              <a id="link" href="#">
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
                 Lagos - NG
               </a>
             </li>
             <li>
-              <a id="link" href="#">
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
                 Los Angeles - USA
               </a>
             </li>
             <li>
-              <a id="link" href="#">
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
                 Bay Area - CA
               </a>
             </li>
             <li>
-              <a id="link" href="#">
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
                 St. Louis - MO
               </a>
             </li>
             <li>
-              <a id="link" href="#">
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
                 Cape Town - ZA
               </a>
             </li>
             <li>
-              <a id="link" href="#">
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
                 London - EN
               </a>
             </li>
             <li>
-              <a id="link" href="#">
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
                 Stockholm - SE
               </a>
             </li>
             <li>
-              <a id="link" href="#">
+              <a
+                id="link"
+                href="https://github.com/Toyehamzat"
+                target="_blank"
+                rel="noopener"
+              >
                 Wien - AU
               </a>
             </li>
